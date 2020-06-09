56e5017f1010c4a1574a323bcf3774b64c564659
MathijsE <48790027+MathijsE@users.noreply.github.com>
2020-06-09 20:17:22 +0200

    ContactType aangepast

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,9 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('naam')
+            ->add('naam', TextType::class)
             ->add('email', EmailType::class)
-            ->add('onderwerp')
+            ->add('onderwerp', TextType::class)
             ->add('bericht', TextareaType::class);
     }
 
